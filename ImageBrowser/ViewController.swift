aa0ceb2f70480ca52d86f3c056e6530b70f58627
ZeroJ <854136959@qq.com>
2016-05-03 23:44:20 +0800

    完成了图片的缩放和手势的添加

--- ImageBrowser/ViewController.swift
+++ ImageBrowser/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let testImageView = ImageView(frame: view.bounds)
+        view.addSubview(testImageView)
+        testImageView.image = UIImage(named: "1")
     }
 
     override func didReceiveMemoryWarning() {
